bfb09c2bcd7ce7d51f35771bfedd398235a6059d
silenceglobally <gisner271@gmail.com>
2026-02-25 20:56:56 -0500

    g

---
+++ js/components/Sidebar/DifficultyInfo.js
@@ -0,0 +1,34 @@
+export default {
+    template: `
+        <h3> </h3>
+        <div class="right-text">
+            <p>
+                 )
+            </p>
+            <p>
+                 
+            </p>
+            <p>
+                
+            </p>
+            <p>
+                
+            </p>
+            <p>
+               
+            </p>
+            <p>
+                
+            </p>
+            <p>
+               
+            </p>
+            <p>
+                
+            </p>
+            <p>
+               
+            </p>
+        </div>
+    `
+}
